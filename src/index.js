7587f3db917323531432f87e92c8434a4aaa22a1
NgunyiGachie <antogachie@gmail.com>
2024-04-02 15:11:13 +0300

    Complete lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let submitButton = document.querySelector('input[value="Create New Task"]')
+
+  let taskContainer = document.getElementById('taskContainer');
+  
+  submitButton.addEventListener('click', function(event){
+    
+    event.preventDefault();
+
+    let taskInput = document.getElementById('new-task-description');
+
+    let task = taskInput.value.trim();
+
+    if (task !== '') {
+      
+      taskContainer.textContent = task;
+    
+    } else {
+      
+      alert("Please enter a task!")
+    
+    }
+    
+    taskInput.value = ''
+  });
+
+  
+
+  const clickableElement = document.getElementById('new-task-description')
+  
+  clickableElement.addEventListener("click", function() {
+    
+    let inputField = document.createElement("input");
+    
+    inputField.type = "text";
+    
+    inputField.placeholder = "Type here";
+    
+    clickableElement.innerHTML = "";
+    
+    clickableElement.appendChild(inputField);
+    
+    inputField.focus();
+    
+    inputField.addEventListener("blur", function() {
+    
+      clickableElement.innerHTML = inputField.value;
+    })
+  })
 });
